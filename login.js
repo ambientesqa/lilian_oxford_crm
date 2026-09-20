3a1c5e50f9e314789743be01f8ec9b6a57ecaff2
ambientesqa <orange.incorporation15@gmail.com>
2026-09-20 15:24:41 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,12 @@
+window.dadosJsonGlobal = {
+    "admin": [
+        ["usuario", "senha"],
+        ["admin", "ed24d2bb07cb85a27ba29c0cd2bba24efab7b6f15d0d7fb19e5765e65fa68d92"]
+    ],
+    "admin_time": "27/07/2026, 20:42:41",
+    "painel": [
+        ["usuario", "senha"],
+        ["consulta", "a2e363fb1596b372d664fa81bf5fbbeb1aaab6e358df85c7973cc5ffbbe82248"]
+    ],
+    "painel_time": "27/07/2026, 20:43:19"
+};
